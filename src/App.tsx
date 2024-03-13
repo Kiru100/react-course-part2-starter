34156ad2f4d4c470c480db5d3c1780fbf38c4941
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-14 03:34:55 +0800

    deleted unused files for authentication

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,16 @@ import './App.css';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
 import { TasksProvider } from './state-management/tasks';
-import { AuthProvider } from './state-management/auth';
 import Counter from './state-management/counter/Counter';
 
 function App() {
  
   return <>
-      <AuthProvider>
         <TasksProvider> 
             <Counter/>
             <NavBar></NavBar>
             <HomePage></HomePage>
         </TasksProvider>
-      </AuthProvider>
     </>;
 }
 
